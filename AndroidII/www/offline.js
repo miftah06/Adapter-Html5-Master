0de4345010d7131710ea4e050024885e1077b009
miftah06 <61154976+miftah06@users.noreply.github.com>
2020-04-09 06:42:18 +0800

    Add files via upload

---
+++ AndroidII/www/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1585810220,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite5-sheet1.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/spritefont.png",
+		"media/die.ogg",
+		"media/hit.ogg",
+		"media/point.ogg",
+		"media/swoosh.ogg",
+		"media/wing.ogg"
+	]
+}
